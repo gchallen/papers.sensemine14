8381cf047f21a0a0c7f661ba35145af0022aebe1
Geoffrey Challen <challen@buffalo.edu>
2012-11-19 15:21:07 -0500

    Refactor.

---
+++ figures/template/graph.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import re,sys,numpy,pickle,argparse
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser()
+parser.add_argument('data')
+parser.add_argument("--min_count", help="Minimum number of values required for inclusion in plot.", action='store', type=int, default=24)
+args = parser.parse_args()
+
+charge_levels = pickle.load(open(args.data, 'rb'))
+
+figure = plt.figure()
+axes = figure.add_subplot(111)
+
+for device in charge_levels.keys():
+  if len(charge_levels[device]) < args.min_count:
+    continue
+  axes.plot([i[0] for i in charge_levels[device]], [i[1] for i in charge_levels[device]])
+
+for label in axes.get_xticklabels():
+  label.set_rotation(30)
+
+figure.savefig('simple.pdf')
