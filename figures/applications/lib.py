8d1efa38724d6941b8c38720e9670134e56e5a50
Geoffrey Challen <challen@buffalo.edu>
2012-11-30 16:19:45 -0500

    Store system apps as well.

--- figures/applications/lib.py
+++ figures/applications/lib.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import cPickle, re, itertools
+import cPickle, re, itertools, datetime
 from common import lib
 
 class Application:
@@ -17,16 +17,28 @@ class Application:
     self.path = path
     self.tags = ['PhoneLabSystemAnalysis-Snapshot', ]
     self.devices = set([])
+    self.start_time = None
+    self.end_time = None
     
     self.applications = set([])
+    self.system_applications = set([])
+    
     self.device_applications = {}
     self.install_counts = {}
     self.coinstalled_applications = lib.AutoDict()
     
     self.popular_installs = []
     
-  def process(self):
+  def process(self, time_limit=None):
     for logline in lib.LogFilter(self.tags).generate_loglines():
+      
+      if self.start_time == None:
+        self.start_time = logline.datetime
+      self.end_time = logline.datetime
+      
+      if time_limit != None and (self.end_time - self.start_time) >= time_limit:
+        break
+      
       if logline.log_tag == 'PhoneLabSystemAnalysis-Snapshot' and logline.json != None and logline.json.has_key('InstalledUserApp'):
         if not self.device_applications.has_key(logline.device):
           self.device_applications[logline.device] = set([])
@@ -34,12 +46,18 @@ class Application:
         
         if not self.install_counts.has_key(application):
           self.install_counts[application] = 0
-        self.install_counts[application] += 1
         
+        if not application in self.device_applications[logline.device]:
+          self.install_counts[application] += 1
+          self.applications.add(application)
+          
         self.device_applications[logline.device].add(application)
-        self.applications.add(application)
+      elif logline.log_tag == 'PhoneLabSystemAnalysis-Snapshot' and logline.json != None and logline.json.has_key('InstalledSystemApp'):
+        application = Application.PACKAGENAME_PATTERN.search(logline.log_message).group('packagename').strip()
+        self.system_applications.add(application)
+        
       self.devices.add(logline.device)
-    
+      
     for first_application, second_application in itertools.combinations(sorted(self.applications), 2):
       self.coinstalled_applications[first_application][second_application] = 0
     
@@ -47,12 +65,12 @@ class Application:
       for first_application, second_application in itertools.combinations(sorted(self.device_applications[device]), 2):
         self.coinstalled_applications[first_application][second_application] += 1
     
-    self.popular_installs = [app for app in sorted(self.install_counts.values(), key=lambda k: self.install_count[k]) if app not in self.PHONELAB_APPS]
+    self.popular_installs = [app for app in reversed(sorted(list(self.applications), key=lambda k: self.install_counts[k])) if app not in self.PHONELAB_APPS]
                                                    
   def dump(self):
     cPickle.dump(self, open(self.path, 'wb'), cPickle.HIGHEST_PROTOCOL)
 
 if __name__=="__main__":
   t = Application('data.dat')
-  t.process()
+  t.process(datetime.timedelta(hours=2))
   t.dump()
